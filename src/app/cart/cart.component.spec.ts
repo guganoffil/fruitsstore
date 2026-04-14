0ce2a4599bbd472bcaf7780fbabb661a5c6421ca
gugan <guganoffil@gmail.com>
2026-04-15 00:07:01 +0530

    feat: inject CartService into CartComponent

--- src/app/cart/cart.component.spec.ts
+++ src/app/cart/cart.component.spec.ts
@@ -1,25 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CartComponent } from './cart.component';
+import { CartService } from '../cart.service';
+import { ThemeService } from '../theme.service';
 
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+  let cartService: CartService;
 
   beforeEach(async () => {
+    localStorage.clear();
     await TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
-    })
-    .compileComponents();
+      declarations: [CartComponent],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [CartService, ThemeService]
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
+    cartService = TestBed.inject(CartService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('updateQuantity should delegate to CartService', (done) => {
+    cartService.addToCart('apple', 'Apple', 30);
+    component.updateQuantity('apple', 1);
+    cartService.items$.subscribe(items => {
+      expect(items[0].quantity).toBe(2);
+      done();
+    });
+  });
+
+  it('removeFromCart should delegate to CartService', (done) => {
+    cartService.addToCart('apple', 'Apple', 30);
+    component.removeFromCart('apple');
+    cartService.items$.subscribe(items => {
+      expect(items.length).toBe(0);
+      done();
+    });
   });
 });
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ThemeService, Theme } from '../theme.service';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
+  items$ = this.cartService.items$;
+  totalPrice$ = this.cartService.totalPrice$;
 
-  constructor() { }
+  constructor(private themeService: ThemeService, private cartService: CartService) {}
 
-  ngOnInit(): void {
+  get theme(): Theme { return this.themeService.theme; }
+
+  onThemeChange(value: string) {
+    this.themeService.setTheme(value as Theme);
   }
 
+  updateQuantity(id: string, delta: number) {
+    this.cartService.updateQuantity(id, delta);
+  }
+
+  removeFromCart(id: string) {
+    this.cartService.removeFromCart(id);
+  }
 }
